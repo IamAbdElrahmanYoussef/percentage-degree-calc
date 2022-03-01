f435c3b32d56e772dacce5e5ce326ed6f4f3c420
IamAbdElrahmanYoussef <100532937+IamAbdElrahmanYoussef@users.noreply.github.com>
2022-03-01 17:16:42 +0200

    Add files via upload
    this is a python script to calc your degree in percentage easily
    just run the script 
    then add the full degree
    then put your degree and hit enter

---
+++ percentage calculator.py
@@ -0,0 +1,8 @@
+print ('percentage calculator')
+fullsum = int (input('Enter the full sum :  '))
+sum = int (input('Enter the sum you got it :  '))
+def calc():
+    multi = sum*100
+    dev = multi/fullsum
+    print('your Degree is :',dev,'%' )
+calc() 
